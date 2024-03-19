73902e72726ee06ace5d50f0792915881b6a98d8
Dino Thiago <thiagocladino@gmail.com>
2024-03-18 21:17:39 -0300

    tela de login do sistema de barbearia

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function login() {
+    // Lógica para fazer login
+}
+
+function clearForm() {
+    document.getElementById('user
+
+function login() {
+    // Lógica para fazer login
+}
+
+function clearForm() {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+}
+
+function exit() {
+    // Lógica para sair
+}
